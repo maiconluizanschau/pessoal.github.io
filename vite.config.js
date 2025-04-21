573722e32d856842238a0f584526a1230fe36e32
Maicon Luiz Anschau <48734952+maiconluizanschau@users.noreply.github.com>
2025-04-21 10:25:53 -0300

    Atualizacao deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/maicon-site/',
+  base: '/maiconluizanschau/',
   plugins: [react()],
 });

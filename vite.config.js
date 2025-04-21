d16cd139bf8d00ce045a3f192c7c4f3d4bebdb18
Maicon Luiz Anschau <48734952+maiconluizanschau@users.noreply.github.com>
2025-04-21 10:27:07 -0300

    url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/maiconluizanschau/',
+  base: 'https://maiconluizanschau.github.io/pessoal.github.io/',
   plugins: [react()],
 });
